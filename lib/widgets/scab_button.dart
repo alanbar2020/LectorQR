82e25a786ee2a6b129bba5885a838b8737eb4308
Alan <alanbarboza2012@gmail.com>
2021-02-23 17:08:44 -0300

    Agregado el qr y comenzando con sqlite terminada clase 157

--- lib/widgets/scab_button.dart
+++ lib/widgets/scab_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButton extends StatelessWidget {
   @override
@@ -6,7 +7,11 @@ class ScanButton extends StatelessWidget {
     return FloatingActionButton(
       child: Icon(Icons.filter_center_focus),
       elevation: 0,
-      onPressed: () {},
+      onPressed: () async {
+        //String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#1e057b', 'Cancelar', false, ScanMode.QR);
+        final barcodeScanRes = 'google.com';
+        print(barcodeScanRes);
+      },
     );
   }
 }
